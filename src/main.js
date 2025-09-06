6dd3998627fd131f278100736925b6692d71f203
DragonTTV <maddragon548@gmail.com>
2025-09-06 17:53:51 +0530

    Added Theatre and Test Cat 3D Model

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias:true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -11,10 +12,24 @@ camera.position.z = 100
 const scene = new THREE.Scene();
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
+
+const ambiLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambiLight)
+
 const cubemat = new THREE.MeshNormalMaterial()
 const cubegeom= new THREE.BoxGeometry(5,5,5)
 const cube = new THREE.Mesh(cubegeom,cubemat)
+cube.position.set(15,0,0)
+console.log(cube.position)
 scene.add(cube)
+const gltfLoader = new GLTFLoader()
+gltfLoader.load("/src/model/theater/scene.gltf", (gltfscene) => {
+    scene.add(gltfscene.scene)
+    gltfscene.scene.scale.set(15,15,15)
+    
+})
+
+
 //Something
 
 function animate(){
