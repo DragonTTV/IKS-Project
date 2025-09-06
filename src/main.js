9ee6775f3cbf5ad1163136ce7ac74c8c36fef749
DragonTTV <maddragon548@gmail.com>
2025-09-06 13:44:06 +0530

    changed the file path for index.html and added a cube in main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import * as THREE from "three";
-
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-const scene = new THREE.Scene();
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias:true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 100
+
+const scene = new THREE.Scene();
+const controls = new OrbitControls(camera, renderer.domElement)
+//Adding a cube
+const cubeGeometry = new THREE.BoxGeometry(10, 10, 10)
+const cubeTexture = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(cubeGeometry, cubeTexture)
+
+scene.add(cube)
+
+function animate(){
+    requestAnimationFrame(animate)
+    // console.log(camera.position)
+    
+    renderer.render(scene, camera)
+}
+
+animate()
