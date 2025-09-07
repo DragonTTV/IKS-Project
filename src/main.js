357d017437552776e8e393ce9cc6765b7447ceac
roop <roop25327@gmail.com>
2025-09-07 22:49:58 +0530

    none

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { loadModels } from "./handler/modelHandler";
-
+//renderer
 const renderer = new THREE.WebGLRenderer({ antialias:true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
+//camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.z = 100
 
-
+//scene
 const scene = new THREE.Scene();
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
