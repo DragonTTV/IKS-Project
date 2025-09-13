46bbf89be77564a3294c2e37eeda11ea4b4a37c3
DragonTTV <maddragon548@gmail.com>
2025-09-13 19:46:54 +0530

    deploy.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/IKS-Project' : '',
+  // base: process.env.NODE_ENV === 'production' ? '/IKS-Project/' : '',
+  base: "/IKS-Project",
   assetsInclude: ['**/*.glb'],
 });
