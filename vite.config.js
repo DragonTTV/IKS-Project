5f81d34f1d7c254380576cb23892950a7abb755a
DragonTTV <maddragon548@gmail.com>
2025-09-13 19:59:37 +0530

    final fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite';
+import { build, defineConfig } from 'vite';
 
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/IKS-Project/' : '',
-  base: "/IKS-Project",
+  base: process.env.NODE_ENV === 'production' ? '/IKS-Project/' : '',
+  // base: "/IKS-Project",
   assetsInclude: ['**/*.glb'],
+  vite:{
+    build:{
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      }
+    }
+  }
 });
